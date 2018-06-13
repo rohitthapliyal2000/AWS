f027f1692ca39af3e99744d72c07c552809651e2
Rohit Thapliyal <rohitthapliyal2000@geu.ac.in>
2018-06-13 14:45:00 +0530

    Updation

--- rekognition/driver.py
+++ rekognition/driver.py
@@ -1,13 +1,19 @@
+from flask import Flask, request, jsonify
 import boto3
 import GET_img
 import json
-from pprint import pprint
+import time
+
+app = Flask(__name__)
 
 def get_labels(thumbImg, client):
 
-	img_size = GET_img.get_image_from_url(thumbImg)
-
 	labels = []
+
+	if len(thumbImg) == 0:
+		return labels
+
+	img_size = GET_img.get_image_from_url(thumbImg)
 
 	try:
 		resp = client.detect_labels(Image={'Bytes': img_size}, MinConfidence=60.0)
@@ -15,46 +21,67 @@ def get_labels(thumbImg, client):
 		for label in resp['Labels']:
 			labels.append(label['Name'])
 
-	except :
+	except Exception as err:
+		labels.append("err")
+		err = str(err)
+		labels.append(err)
 		return labels
 
 	return labels
 
+@app.route("/", methods=["POST"])
+def feed():
+	data = request.json
 
-with open('data.json') as f:
-	data = json.load(f)
+	start = time.time()
+	with open("cre") as file:
+		for line in file:
+			if line[0] == 'A':
+				ACCESS_KEY = line
+			else:
+				SECRET_KEY = line
 
-with open("cre") as f:
-	for line in f:
-		if line[0] == 'A':
-			ACCESS_KEY = line
-		else:
-			SECRET_KEY = line
+	ACCESS_KEY = ACCESS_KEY[:len(ACCESS_KEY) - 1]
 
-ACCESS_KEY = ACCESS_KEY[:len(ACCESS_KEY) - 1]
+	client = boto3.client('rekognition', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 
-client = boto3.client('rekognition', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+	form = []
 
-form = []
+	for iterator in range(0, len(data)):
 
-for iterator in range(0, len(data["data"]["hits"])):
+		unit = {}
+		mageId = data[iterator]["mageId"]
+		thumbImg = data[iterator]["thumbImg"]
 
-	unit = {}
+		if len(thumbImg) == 0:
+			continue
+		
+		union = []
 
-	mageId = data["data"]["hits"][iterator]["mageId"]
-	thumbImg = data["data"]["hits"][iterator]["thumbImg"]
+		for link in thumbImg:
+			labels = get_labels(link, client)
+			if labels[0] == "err":
+				unit["error"] = []
+				unit["error"].append(labels[1])
+			elif len(union) == 0:
+				union = labels
+			else:
+				for label in labels:
+					if label not in union:
+						union.append(label)
+		
+		unit["mageId"] = mageId
+		unit["tags"] = union
 
-	if len(thumbImg) == 0:
-		continue
-	labels = get_labels(thumbImg, client)
-	
-	unit["mageId"] = mageId
-	unit["tag"]	= labels
+		form.append(unit)
 
-	form.append(unit)
 
-pprint(form)
+	return jsonify(form)
 
+
+if __name__ == "__main__":
+	app.run(debug=True)
+# Output
 # [
 # 	{
 # 		mageID: 
